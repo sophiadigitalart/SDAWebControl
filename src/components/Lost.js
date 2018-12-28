075a61c6afcc73e001510c59bdca8e2f2d78c47d
Bruce LANE <bruce@batchass.fr>
2018-12-28 19:26:25 +0100

    bootstrap

---
+++ src/components/Lost.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class Lost extends Component {
+    render() {
+        return (
+            <h1>Lost 404</h1>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
+import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import App from './App';
+import Controls from './components/Controls';
+import Lost from './components/Lost';
 import * as serviceWorker from './serviceWorker';
 
 //const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 
+ReactDOM.render((
+<Router basename={"/"}>
+    <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/controls" component={Controls} />
+        <Route component={Lost} />
+    </Switch>
+   
+</Router>
+), document.getElementById('root'));
+
+/*
 ReactDOM.render(<BrowserRouter basename={"/"}>
     <App />
   </BrowserRouter>, document.getElementById('root'));
-
+*/
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
