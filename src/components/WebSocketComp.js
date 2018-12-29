bfc4345925af8b517345ff87876ba61e3a2a70df
Bruce LANE <bruce@batchass.fr>
2018-12-29 21:26:28 +0100

    CodeMirror start

--- src/components/WebSocketComp.js
+++ src/components/WebSocketComp.js
@@ -1,14 +1,24 @@
 ﻿// from https://medium.com/practo-engineering/websockets-in-react-the-component-way-368730334eef
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/lib/codemirror.css';
 
 export default class WebSocketComp extends Component {
     constructor(props) {
         super(props);
-        this.state = { messages: [] };
+        this.state = { 
+            messages: [],
+            code:"// Code\ndvqjdslmkjmjvsdkqjfmdqsjfqdmsjf" 
+        };
     }
     wsSend(asset) {
         this.connection.send(asset.text);
+    }
+    updateCode(newCode) {
+        this.setState({
+            code: newCode,
+        });
     }
     componentDidMount() {
         // this is an "echo" websocket service for testing pusposes
@@ -31,10 +41,16 @@ export default class WebSocketComp extends Component {
     }
     render() {
         
+        var options = {
+            lineNumbers: true,
+        };
         // render the messages from state:
         return (
             <div>
                 <RaisedButton label="Send" onClick={this.wsSend.bind(this,this.props.asset)} />
+
+                <CodeMirror value={this.state.code} onChange={this.updateCode.bind(this,this.props.asset)} options={options} />
+
                 <ul>{this.state.messages.slice(-5).map((msg, idx) => <li key={'msg-' + idx}>{msg}</li>)}</ul>
             </div>
         );
