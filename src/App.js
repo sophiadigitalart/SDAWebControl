844c1d5809f413064692c96dc57472cb8fc19fa7
Bruce LANE <bruce@batchass.fr>
2018-12-30 00:02:21 +0100

    send ws frag ok

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import WebSocketComp from './components/WebSocketComp';
 
 const defaultAsset = {
   _id: 0,
-  name: "AcidAtTheDisco",
-  text: "AcidAtTheDisco{]"
+  name: "def",
+  text: "def"
 }
 export default class App extends Component {
   displayName = App.name
@@ -34,8 +34,20 @@ export default class App extends Component {
           },
           {
               _id: 2,
-              name: "Hx",
-              text: "vec2 uv = gl_FragCoord.xy / RENDERSIZE.xy;"
+              name: "AcidAtTheDisco",
+              text: "// https://www.shadertoy.com/view/4sfXRB \
+              void main(void)\
+              {\
+                vec2 uv = iZoom * gl_FragCoord.xy / iResolution.xy;\
+                float time = iTime;\
+                float depth = sin(uv.y*12.0+sin(time)*1.5+1.0+sin(uv.x*3.0+time*1.2))*cos(uv.y*2.0+time)+sin((uv.x*3.0+time));\
+                float texey = (uv.x-0.5);\
+                float xband = sin(sqrt(uv.y/uv.y)*16.0/(depth)+time*3.0);\
+                float final = (\
+                  sin(texey/abs(depth)*32.0+time*16.0+sin(uv.y*uv.x*32.0*sin(depth*3.0)))*(depth)*xband\
+                );\
+                fragColor = vec4(0.,(-final*sin(time)*2.0),(final),1.0)*1.5;\
+              }"
           },
           {
               _id: 3,
@@ -55,6 +67,7 @@ export default class App extends Component {
 
   }
   updateCurrentAsset(asset) {
+    console.log("updateCurrentAsset:" +JSON.stringify(asset) );
     this.setState({
       currentAsset: asset
     })
--- src/components/WebSocketComp.js
+++ src/components/WebSocketComp.js
@@ -7,22 +7,25 @@ import 'codemirror/lib/codemirror.css';
 export default class WebSocketComp extends Component {
     constructor(props) {
         super(props);
+        console.log("WebSocketComp ctor:" + JSON.stringify(props) );
         this.state = { 
             messages: [],
-            code:"// Code\ndvqjdslmkjmjvsdkqjfmdqsjfqdmsjf" 
-        };
+            code:"// Codej" 
+        };//
+
+        this.updateCode = this.updateCode.bind(this);
     }
     wsSend(asset) {
         this.connection.send(asset.text);
     }
     updateCode(newCode) {
+        console.log(newCode);
         this.setState({
             code: newCode,
         });
+        this.connection.send("{\"event\":\"frag\",\"message\":\"" + this.state.code + "\"}");
     }
     componentDidMount() {
-        // this is an "echo" websocket service for testing pusposes
-        //this.connection = new WebSocket('wss://echo.websocket.org');
         this.connection = new WebSocket('ws://localhost:8088');
         //this.connection = new WebSocket('wss://.northeurope.cloudapp.azure.com/');
         // listen to onmessage event
@@ -32,25 +35,19 @@ export default class WebSocketComp extends Component {
                 messages: this.state.messages.concat([evt.data])
             });
         };
-
         // for testing: sending a message to the echo service every 2 seconds, 
-        // the service sends it right back
-        setInterval(_ => {
-            this.connection.send(Math.random());
-        }, 20000);
+        // setInterval(_ => {
+        //     this.connection.send(Math.random());
+        // }, 20000);
     }
-    render() {
-        
+    render() {        
         var options = {
             lineNumbers: true,
         };
-        // render the messages from state:
         return (
             <div>
                 <RaisedButton label="Send" onClick={this.wsSend.bind(this,this.props.asset)} />
-
-                <CodeMirror value={this.state.code} onChange={this.updateCode.bind(this,this.props.asset)} options={options} />
-
+                <CodeMirror value={this.props.asset.text} onChange={this.updateCode} options={options} />
                 <ul>{this.state.messages.slice(-5).map((msg, idx) => <li key={'msg-' + idx}>{msg}</li>)}</ul>
             </div>
         );
